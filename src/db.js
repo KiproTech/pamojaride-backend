9629d586eb084125d4f346bc1791b5ff4661fd8d
Amos Kiprotich <kiprotich4261069kiprotich@gmail.com>
2026-03-12 01:17:20 +0300

    Fix Railway database connection

--- src/db.js
+++ src/db.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mariadb.createPool({
-  host: process.env.DB_HOST || "turntable.proxy.rlwy.net",
-  user: process.env.DB_USER || "root",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "railway",
-  port: process.env.DB_PORT || 26291,
+  database: process.env.DB_NAME,
   connectionLimit: 5,
   ssl: { rejectUnauthorized: false }
 });
